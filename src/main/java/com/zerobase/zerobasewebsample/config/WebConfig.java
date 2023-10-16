fce26a7d495abfef7ed6f6940e45cf57bca4bfaa
parktj <ekswn120@gmail.com>
2023-10-16 19:34:17 +0900

    Interceptor 처리 간단 예시

--- src/main/java/com/zerobase/zerobasewebsample/config/WebConfig.java
+++ src/main/java/com/zerobase/zerobasewebsample/config/WebConfig.java
@@ -4,18 +4,27 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public FilterRegistrationBean loggingFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/payment");
+        filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "/images/*");
+    }
 }
