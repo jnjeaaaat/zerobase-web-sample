95e366defd82caed17d72886d871a95bdd25762c
parktj <ekswn120@gmail.com>
2023-10-16 15:11:17 +0900

    init commit

---
+++ src/main/java/com/zerobase/zerobasewebsample/ZerobaseWebSampleApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.zerobasewebsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseWebSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerobaseWebSampleApplication.class, args);
+    }
+
+}
