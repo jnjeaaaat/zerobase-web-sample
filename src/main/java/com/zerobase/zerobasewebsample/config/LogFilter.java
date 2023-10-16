d42217917b20587d3e5a4793b1122f989c1b2f63
parktj <ekswn120@gmail.com>
2023-10-16 19:16:26 +0900

    @Configuration 클래스로 filter 처리

--- src/main/java/com/zerobase/zerobasewebsample/config/LogFilter.java
+++ src/main/java/com/zerobase/zerobasewebsample/config/LogFilter.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-
 @Slf4j
-@Component
 public class LogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
---
+++ src/main/java/com/zerobase/zerobasewebsample/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.zerobase.zerobasewebsample.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean loggingFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/payment");
+
+        return filterRegistrationBean;
+    }
+
+}
